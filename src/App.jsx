1012f23b9469e31969c409b37e911a0a29ad8ac7
TinWinHlaing-777 <tiniwinhlaingmm777@gmail.com>
2024-12-05 00:15:44 +0630

    updated issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router";
-import Home from "./pages/home";
+
 import { GlobalStyle } from "./styles/Global.style";
+import Home from "./pages/home";
 
 function App() {
   return (
